5aa64a6ad811516f6c3a463b6029abd8dd3078e5
evanchr <95886771+evanchr@users.noreply.github.com>
2023-03-18 16:32:42 +0100

    Commentaires 2ème partie

--- src/main/scala/interpreter/interpreter.scala
+++ src/main/scala/interpreter/interpreter.scala
@@ -166,10 +166,15 @@ object Interpreter {
    * @return la mémoire après l'interprétation de la liste de commandes
    */
   def interpreterCommands(commands: List[Command], memory: Memory): Memory = {
+    // on execute une commande sur la memoire, et on renvoit la memoire changée
+    // cette méthode sert à éxécuter les listes de commandes
     commands match {
-      case Nil => throw ExceptionListeVide
-      case hd :: Nil => interpreterCommand(hd, memory)
+      case Nil => throw ExceptionListeVide // la liste des commandes est vide
+      case hd :: Nil => interpreterCommand(hd, memory) // il reste une seule commande dans la liste de commande donc on l'éxécute avec interpreterCommand
       case hd :: tl => interpreterCommands(tl, interpreterCommand(hd, memory))
+        // dans ce dernier cas, on interprète la première commande de la liste (hd) qui change une première fois la mémoire
+        // puis on appelle récursivement la méthode afin de traiter la liste des commandes restantes (tl) 
+        // qui prend en paramétre la mémoire déjà changé par la première commande etc.. jusqu'a ce que la liste de commandes soit vide
     }
   }
   
@@ -186,10 +191,13 @@ object Interpreter {
    */
   def interpreterMemorySet(vars: List[Variable], vals: List[Value]): Memory = {
     (vars,vals) match {
-      case (Nil,Nil) => throw ExceptionListeVide
-      case (_,Nil) | (Nil,_) => throw ExceptionListesDeLongueursDifferentes
-      case (hd::Nil,hd2::Nil) => (hd,hd2) :: Nil
+      case (Nil,Nil) => throw ExceptionListeVide // cas où les 2 listes sont vides
+      case (_,Nil) | (Nil,_) => throw ExceptionListesDeLongueursDifferentes // cas où une des 2 listes se termine avant l'autre
+      case (hd::Nil,hd2::Nil) => (hd,hd2) :: Nil // cas où il y a uniquement 1 variable à associer à 1 valeur
       case (hd::tl,hd2::tl2) => (hd,hd2) :: interpreterMemorySet(tl,tl2)  
+        // dans ce dernier cas, on crée une mémoire avec comme premier élément (variable,valeur) associé
+        // puis on fait un appel récursif sur le reste des variables et valeurs à associer
+        // on concatène cet appel au premier élément
     }
   }
   
@@ -201,9 +209,12 @@ object Interpreter {
    */
   def interpreterMemoryGet(vars: List[Variable], memory: Memory): List[Value] = {
     vars match {
-      case Nil => throw ExceptionListeVide
-      case head :: Nil => lookUp(head, memory) :: Nil
+      case Nil => throw ExceptionListeVide // cas où la liste des variables à chercher est vide
+      case head :: Nil => lookUp(head, memory) :: Nil // cas où l'on cherche une seule variable
       case head :: next => lookUp(head, memory) :: interpreterMemoryGet(next, memory)
+        // dans ce dernier cas, on cherche avec la méthode lookUp la première variable dans la mémoire
+        // puis on fait un appel récursif sur le reste des variables à rechercher
+        // on concatène cet appel au premier élément afin de créer une liste de valeurs à retourner
     }
   }
 
@@ -213,11 +224,12 @@ object Interpreter {
    * @return la liste des valeurs des variables de sortie
    */
   def interpreter(program: Program, vals: List[Value]): List[Value] = {
+    // on cherche ici à recréer le programme en fonction de toutes les méthodes
     program match {
       case Progr(in, body, out) => 
-        var mem = interpreterMemorySet(in,vals)
-        mem = interpreterCommands(body, mem)
-        interpreterMemoryGet(out,mem)
+        var mem = interpreterMemorySet(in,vals) // on affecte les variables d'entrée à leurs valeurs
+        mem = interpreterCommands(body, mem) // on change la mémoire en fonction de la liste de commandes à effectuer
+        interpreterMemoryGet(out,mem) // on récupère les variables avec leurs nouvelles valeurs de sortie
     }
   }
   
